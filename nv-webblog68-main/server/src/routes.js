e25322532a5618c499f84f341f966bddb76ae244
kittiya mee <threechompoo@gmail.com>
2025-12-25 19:50:34 +0700

    Delete nv-webblog68-main/server directory

--- nv-webblog68-main/server/src/routes.js
+++
@@ -1,9 +0,0 @@
-const DrinkController = require('./controllers/DrinkController');
-
-module.exports = (app) => {
-  app.get('/drinks', DrinkController.index);
-  app.get('/drink/:id', DrinkController.show);
-  app.post('/drink', DrinkController.create);
-  app.put('/drink/:id', DrinkController.update);
-  app.delete('/drink/:id', DrinkController.delete);
-};
