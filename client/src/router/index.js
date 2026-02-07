90f5b1fa1fd5cd848b37a9590cb0d939e73bc0b8
Kittiya Mee <threechompoo@gmail.com>
2026-02-07 14:04:32 +0700

    add blogs components and update router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// Import Components
+import UserIndex from '../components/Users/Index.vue'
 import UserCreate from '../components/Users/CreateUser.vue'
 import UserEdit from '../components/Users/EditUser.vue'
 import UserShow from '../components/Users/ShowUser.vue'
-import UserIndex from '../components/Users/Index.vue'
-import Login from '@/components/Login.vue'
+import Login from '../components/Login.vue'
+
+// Blogs
+import BlogIndex from '../components/Blogs/Index.vue'
+import BlogCreate from '../components/Blogs/CreateBlog.vue'
+import BlogEdit from '../components/Blogs/EditBlog.vue'
+import BlogShow from '../components/Blogs/ShowBlog.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(), // [5]
   routes: [
-    {
-    path: '/login',
-    name: 'login',
-    component: Login
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
-    },
     {
       path: '/users',
       name: 'users',
@@ -37,8 +35,35 @@ const router = createRouter({
       path: '/user/:userId',
       name: 'user',
       component: UserShow
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    // blogs
+    {
+        path: '/blogs',
+        name: 'blogs',
+        component: BlogIndex
+    },
+    {
+        path: '/blog/create',
+        name: 'blogs-create', // แก้ไขชื่อให้สื่อความหมาย
+        component: BlogCreate
+    },
+    {
+        path: '/blog/edit/:blogId',
+        name: 'blog-edit',
+        component: BlogEdit
+    },
+    {
+        path: '/blog/:blogId',
+        name: 'blog',
+        component: BlogShow
     }
-  ]
+  ],
 })
 
 export default router
+
